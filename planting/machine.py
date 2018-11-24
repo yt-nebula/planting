417a4f17d54a8d19c666ee66c3188d5cd9c6c445
snowdrop <yitu@yitudeMacBook-Air-3.local>
2018-11-24 14:50:51 +0800

    update

--- planting/machine.py
+++ planting/machine.py
@@ -56,7 +56,7 @@ class Machine(object):
         module.register_machine(self)
 
 if __name__ == '__main__':
-    node1 = Machine("127.0.0.1", "root", "root")
+    node1 = Machine("127.0.0.1", "root", "1234")
     node1.register(operations.download.Download)
     node1.download(url="www.baidu.com", dest="~/")
 
