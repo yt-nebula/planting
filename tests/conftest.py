dea371f519d0aa3adf83f1aebc473863f062253b
ge.wu <ge.wu@yitu-inc.com>
2018-11-24 11:05:13 +0800

    add conftest

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import pytest
+from tests.docker_machine import start_docker, kill_docker
+
+@pytest.fixture(scope="session")
+def dokcer_machine(self, request):
+    self.docker = start_docker()
+
+    def fin():
+        kill_docker(self.dokcer.docker_id)
+
+    request.addfinalizer(fin)
