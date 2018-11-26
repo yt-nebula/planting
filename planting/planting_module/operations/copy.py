8a82137e6ba626494167fc0bfaa93a572f0d6c0e
rfkimi <shaobin@shaobindeMBP.lan>
2018-11-26 21:21:17 +0800

    modify module

--- planting/planting_module/operations/copy.py
+++ planting/planting_module/operations/copy.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from environment import Environment
 from planting_module import ModuleBase
-from planting_api_v1 import PlantingApi
 
 
-class Port(ModuleBase):
+class Copy(ModuleBase):
     def __init__(self):
-        super(Port, self).__init__()
+        super(Copy, self).__init__()
 
-    def build_tasks(self, port):
+    def build_tasks(self, src, dest):
         self._tasks = [dict(action=dict(
-            module='port',
-            args=port))]
+            module='copy',
+            args=dict(src=src, dest=dest)))]
 
     def output_field(self):
         self._output = 'msg'
@@ -21,8 +19,8 @@ class Port(ModuleBase):
     def register_machine(self, machine):
         self._env = machine._env
         self._planting = machine._planting
-        machine.port = self
+        machine.download = self
 
-    def __call__(self, port):
-        self.build_tasks(port)
+    def __call__(self, src, dest):
+        self.build_tasks(src, dest)
         self.play()
--- setup.py
+++ setup.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+from setuptools import setup, find_packages
+
+
+package_name = "planting"
+packages = find_packages(
+    include=[package_name, "{}.*".format(package_name)]
+)
+
+setup(
+    name="Planting",
+    version="1.0.a",
+    description='Next generation deployment tool fully depended on Ansible.',
+    license='BSD License',
+    packages=packages,
+    platforms=["all"],
+    url='https://github.com/yt-nebula/planting',
+    install_requires=[
+        'ansible==2.5.0',
+        'colorama',
+    ],
+    classifiers=[
+        "Operating System :: linux",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
