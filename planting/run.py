5b76e6942e7c91802ea12a0d6ab1042e4b205c4d
潘建龙 <panjianlong@yitudeMacBook-Pro-60.local>
2018-11-30 21:37:06 +0800

    ...

--- planting/run.py
+++ planting/run.py
@@ -5,7 +5,7 @@ from machine import Machine
 from planting_module import operations
 
 if __name__ == '__main__':
-    node = Machine('10.40.50.132', 'linuxadmin', 'Hello=111!')
+    node = Machine('xxx.xxx.xxx.xxx', 'xxxx', 'xxxx')
     node.register_all()
     node.list_all_module()
     node.create(path="~/dir", state="dir")
