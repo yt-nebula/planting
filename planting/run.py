842da36f21872aa71626ceddbd60559ab40dc066
潘建龙 <panjianlong@yitudeMacBook-Pro-60.local>
2018-12-01 14:49:10 +0800

    格式

--- planting/run.py
+++ planting/run.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+import os
+os.sys.path.append('/Users/panjianlong/github/planting/')
+
 
 from machine import Machine
 from planting.planting_module import operations
 
 if __name__ == '__main__':
-    node = Machine('xxx.xxx.xxx.xxx', 'xxxx', 'xxxx')
+    node = Machine('10.40.50.132', 'linuxadmin', 'Hello=111!', python='/usr/bin/python')
     node.register_all()
     node.list_all_module()
-    node.create(path="~/dir", state="dir")
-    node.create(path="~/dir/1.txt", state="file")
-    node.jsoninfile(
-        path="~/cluster_config.json", 
-        keys=["machine_list", 0, "resource_capacity"], 
-        val="xxxx")
-    node.jsoninfile(
-        path="~/cluster_config.json", 
-        keys=["machine_list", 1], 
-        val="xxxx")
-    #  node.create(path="~/dir", state="dir")
+    # node.create(path="~/dir", state="dir")
     # node.create(path="~/dir/1.txt", state="file")
-    #
+    # node.jsoninfile(
+    #     path="~/test.conf", 
+    #     keys=["cluster", "list", 0], 
+    #     val={"ee": 22})
+
