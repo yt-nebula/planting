ec011b2b803a46f2b63770ccdd56fa517f0f0671
rfkimi <shaobin@shaobindeMBP.lan>
2018-12-05 19:20:46 +0800

    modify wait for api return result

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -65,6 +65,9 @@ def test_pip(machine: Machine):
 
 def test_waitfor(machine: Machine):
     assert True is machine.wait_for(port='22', state='started', timeout=10)
+    # FIXME: can't test close port due to missing nginx
+    # machine.shell(command='nginx -c /usr/local/nginx/conf/nginx.conf')
+    # assert True is machine.wait_for(port='80', state='started', timeout=10)
 
 # FIXME: can't test in docker due to missing GNU tar
 # def test_unarchive(machine: Machine):
