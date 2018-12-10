0a599873cc54467c074d31f84ae19a0e5bb1a777
Snowdrop <xiangfulin@outlook.com>
2018-12-10 20:41:03 +0800

    mkdir before setup logging config

--- planting/__init__.py
+++ planting/__init__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+import os
+
 import logging.config
 
 from planting.logger import LOGGING
 
+os.makedirs('logs', exist_ok=True)
 logging.config.dictConfig(LOGGING)
